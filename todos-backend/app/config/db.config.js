7b37383d2b26ada159288017d2bf21e0bfc9597a
Michael Ngiri <michaelngiri@gmail.com>
2023-01-29 21:48:16 +0100

    update db with live info

--- todos-backend/app/config/db.config.js
+++ todos-backend/app/config/db.config.js
@@ -1,18 +1,3 @@
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "postgres",
-//   PASSWORD: "postgres",
-//   // DB: process.env.NODE_ENV === "test" ? "todos-test" : "todos",
-//   DB: "todos",
-//   dialect: "postgres",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
-
 module.exports = {
   HOST: "jelani.db.elephantsql.com",
   USER: "vzuxbsfz",
